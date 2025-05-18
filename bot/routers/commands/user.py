77216da171e0a587068e78e210b9c7e7f1134c25
welaskez <nazar.nikitenko.03@mail.ru>
2025-05-18 17:33:25 +0500

    feat: handled start cmd
    - added user schemas
    - added message texts

--- bot/routers/commands/user.py
+++ bot/routers/commands/user.py
@@ -1,3 +1,16 @@
-from aiogram import Router
+from aiogram import Router, types
+from aiogram.filters import CommandStart
+from core import message_texts
+from core.schemas.user import UserCreate
+from services.user import UserService
 
 router = Router(name=__name__)
+
+
+@router.message(CommandStart())
+async def start_cmd(message: types.Message, user_service: UserService):
+    user = await user_service.get_by_tg_id(tg_id=message.from_user.id)
+    if not user:
+        await user_service.create(UserCreate(tg_id=message.from_user.id))
+
+    await message.answer(text=message_texts.START)
