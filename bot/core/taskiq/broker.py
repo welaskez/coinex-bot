eac75ab33fe1eed1a70b1c421df1bb4a6a67c674
welaskez <nazar.nikitenko.03@mail.ru>
2025-05-18 19:54:24 +0500

    feat: added broker state
    - added coinex api to broker state
    - fixed broker url

--- bot/core/taskiq/broker.py
+++ bot/core/taskiq/broker.py
@@ -1,11 +1,25 @@
 import taskiq_aiogram
-from taskiq import TaskiqScheduler
+from taskiq import TaskiqEvents, TaskiqScheduler, TaskiqState
 from taskiq.schedule_sources import LabelScheduleSource
 from taskiq_aio_pika import AioPikaBroker
+from utils.coinex_api import CoinexAPI
 
 from core.config import settings
 
-broker = AioPikaBroker(url=settings.rmq.url)
+broker = AioPikaBroker(url=str(settings.rmq.url))
 scheduler = TaskiqScheduler(broker, sources=[LabelScheduleSource(broker)])
 
 taskiq_aiogram.init(broker, dispatcher="main:dp", bot="main:bot")
+
+
+@broker.on_event(TaskiqEvents.WORKER_STARTUP)
+async def startup(state: TaskiqState) -> None:
+    state.coinex_api = CoinexAPI(
+        base_url=settings.coinex.base_url,
+        headers={"X-Request-Id": settings.coinex.api_key},
+    )
+
+
+@broker.on_event(TaskiqEvents.WORKER_SHUTDOWN)
+async def shutdown(state: TaskiqState) -> None:
+    await state.coinex_api.close()
