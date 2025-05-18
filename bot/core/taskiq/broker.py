c59ac4234fa7600837512e01060fa49963dbd0ae
welaskez <nazar.nikitenko.03@mail.ru>
2025-05-18 18:50:59 +0500

    feat: setup taskiq-aiogram

--- bot/core/taskiq/broker.py
+++ bot/core/taskiq/broker.py
@@ -1,3 +1,4 @@
+import taskiq_aiogram
 from taskiq import TaskiqScheduler
 from taskiq.schedule_sources import LabelScheduleSource
 from taskiq_aio_pika import AioPikaBroker
@@ -6,3 +7,5 @@ from core.config import settings
 
 broker = AioPikaBroker(url=settings.rmq.url)
 scheduler = TaskiqScheduler(broker, sources=[LabelScheduleSource(broker)])
+
+taskiq_aiogram.init(broker, dispatcher="main:dp", bot="main:bot")
